ded6fe83a736669931c30e83c747cd01322f40aa
Emanuel Barrera <emanuelbarrera98@gmail.com>
2023-08-31 19:17:01 -0600

    Create Object1.java
    Add model

---
+++ FetchHomeTest/app/src/main/java/com/main/app/model/Object1.java
@@ -0,0 +1,31 @@
+package com.main.app.model;
+
+public class Object1 {
+    private int id;
+    private int listId;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
